424b34db1b6e368901924f9410d929ffd2c8b4ca
Oliver M. Harris <omharris@protonmail.com>
2025-10-14 15:13:27 +0100

    L23 complete

--- C1/L23.go
+++ C1/L23.go
@@ -3,9 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	senderName = "Syl"
-	recipient = "Kaladin"
-	message = "The Words, Kaladin. You have to speak the Words!"
+	senderName := "Syl"
+	recipient := "Kaladin"
+	message := "The Words, Kaladin. You have to speak the Words!"
 
 	fmt.Printf("%s to %s: %s\n", senderName, recipient, message)
 }
